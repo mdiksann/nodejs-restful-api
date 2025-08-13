4b085cef75f57e3bc77cb963337e900a49b96f2e
iksan <mdiksann@gmail.com>
2025-08-13 19:19:11 +0700

    feat: add address module (controller, service, validation, tests)

--- src/route/api.js
+++ src/route/api.js
@@ -2,7 +2,7 @@ import express from "express";
 import userController from "../controller/user-controller.js";
 import {authMiddleware} from "../middleware/auth-middleware.js";
 import contactController from "../controller/contact-controller.js";
-
+import addressController from "../controller/address-controller.js";
 
 //USER API Routes
 const userRouter = new express.Router();
@@ -17,6 +17,13 @@ userRouter.get('/api/contacts/:contactId', contactController.get);
 userRouter.put('/api/contacts/:contactId', contactController.update);
 userRouter.delete('/api/contacts/:contactId', contactController.remove);
 userRouter.get('/api/contacts', contactController.search);
+
+//ADDRESS API ROUTES
+userRouter.post('/api/contacts/:contactId/addresses', addressController.create);
+userRouter.get('/api/contacts/:contactId/addresses/:addressId', addressController.get);
+userRouter.put('/api/contacts/:contactId/addresses/:addressId', addressController.update);
+userRouter.delete('/api/contacts/:contactId/addresses/:addressId', addressController.remove);
+userRouter.get('/api/contacts/:contactId/addresses', addressController.list);
 
 export {
     userRouter
